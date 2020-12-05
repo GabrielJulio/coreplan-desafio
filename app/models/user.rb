aba9f6d8876ca4a834dcb347f6ac5464f34aa15a
Gabriel Júlio <gabrieljuliobs@gmail.com>
2020-12-05 08:48:59 -0300

    Adicionado validações ao model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
   after_commit :add_default_avatar, on: %i[create update]
+  validates :full_name, :email, :nickname, :birth_date, presence: :true
+  validates :email, :nickname, uniqueness: { case_sensitive: false }
+  validate :minimum_age
 
   def avatar_thumbnail
     if avatar.attached?
@@ -15,6 +18,11 @@ class User < ApplicationRecord
   end
 
   private
+  def minimum_age
+    if birth_date.present? && birth_date > 16.years.ago.to_date
+      errors.add(:birth_date, 'You should be over 16 years old.')
+    end
+  end
   def add_default_avatar
     unless avatar.attached?
       avatar.attach(
