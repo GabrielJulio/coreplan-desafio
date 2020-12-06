31486abcf1d14b2390d055261400316d30bea98d
Gabriel Júlio <gabrieljuliobs@gmail.com>
2020-12-06 10:43:30 -0300

    User adicionado em dev:setup + Refatoração

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
     protected
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :full_name, :nickname, :birth_date,])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :full_name, :nickname, :birth_date,])
     end
 end
